4549c014e5838c801eca1ff03d6eefd20e74930a
TacoToads <enhminecraft@gmail.com>
2025-10-20 18:22:18 +0000

    taco

--- Main.java
+++ Main.java
@@ -6,13 +6,67 @@ public class Main
   {
     // write solutions to problems here
 
-    int count = 3;
+    // int count = 3;
 
-    while (count <= 25)
+    // while (count <= 25)
+    // {
+    //   System.out.println(count);
+    //   count = count + 2;
+    // }
+
+
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter positive numbers to add. Enter -1 to stop.");
+    // int sum = 0;
+    // int userInput = 0;
+
+    // while (userInput != -1)
+    // {
+    //   userInput = sc.nextInt();
+    //   sum += userInput;
+    // }
+    // sum++; //Note: When the user enters -1, we add -1 to the sum
+    //       // so we ++ to add back 1
+    // System.out.println("The sum is " + sum);
+
+
+    //Problem 2
+
+    // int N;
+    // System.out.println("How many numbers do you want to enter?");
+    // N = sc.nextInt();
+    // int max = Integer.MIN_VALUE; //int max = Integer.MIN_VALUE;
+
+    // int counter = 0;
+    // while (counter < N);
+    // {
+    //   userInput = sc.nextInt();
+    //   if(userInput > max)
+    //   {
+    //      max = userInput;
+    //   }
+    //   counter ++;
+
+    //   System.out.println("The max is " + max);
+
+    // }
+
+    String word = "calculator";
+    int index = 0;
+    int counter = 0;
+    while (index < word.length()) //Get every letter loop
     {
-      System.out.println(count);
-      count = count + 2;
+      String currentLetter = word.substring(index, index+1); 
+      if(counter % 3 != 0)
+      {
+        System.out.println(currentLetter);
+      }
     }
+     
+
+    index++;
+    counter++;
+    // sc.close();
 
   }
 }
